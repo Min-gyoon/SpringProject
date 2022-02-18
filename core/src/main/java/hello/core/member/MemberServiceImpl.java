ba57832cbb85cd3611eb4b412a6222dcbce4b661
Jeon Min Gyoon <jmgendrmf@gmail.com>
2022-02-18 14:00:04 +0900

    22018

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+	private final MemberRepository memberRepository = new MemoryMemberRepositioy();
+	
+	
+	
+	@Override
+	public void join(Member member) {
+		// TODO Auto-generated method stub
+		memberRepository.save(member);
+	}
+
+	@Override
+	public Member findMember(Long memberId) {
+		// TODO Auto-generated method stub
+		return memberRepository.findById(memberId);
+	}
+
+}
